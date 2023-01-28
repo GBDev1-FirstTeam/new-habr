59c85cc3ba110e3c72b5a3530e7231bc7f80f180
Ivan-Dell <vanbog@mail.ru>
2023-01-28 21:59:03 +0300

    created interfaces of basic repositories

---
+++ 03. DAL/NewHabr.DAL/Repository/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace NewHabr.DAL.Repository;
+public interface IRepository<T,Tkey>
+{
+    List<T> GetAll();
+    T GetById(Tkey id);
+    Tkey Create(T data);
+    int Update(T data);
+    int Delete(Tkey id);
+}
