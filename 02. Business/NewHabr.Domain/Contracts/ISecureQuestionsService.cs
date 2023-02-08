1af45306787466dbd2495df3e1c46639f3a62811
Simeon Iksanov <simeon.iksanov@gmail.com>
2023-02-08 23:19:22 +0500

    fixes after review

--- 02. Business/NewHabr.Domain/Contracts/ISecureQuestionsService.cs
+++ 02. Business/NewHabr.Domain/Contracts/ISecureQuestionsService.cs
@@ -6,7 +6,7 @@ namespace NewHabr.Domain.Contracts;
 
 public interface ISecureQuestionsService
 {
-    Task<ICollection<SecureQuestionDto>> GetAllAsync(bool trackChanges, CancellationToken cancellationToken);
+    Task<ICollection<SecureQuestionDto>> GetAllAsync(CancellationToken cancellationToken);
     Task CreateAsync(SecureQuestionCreateRequest request, CancellationToken cancellationToken);
     Task UpdateAsync(int id, SecureQuestionUpdateRequest request, CancellationToken cancellationToken);
     Task DeleteAsync(int id, CancellationToken cancellationToken);
