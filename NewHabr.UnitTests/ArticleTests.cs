c8be40d5e9bb3d4207586324522d0ac7059c97d6
EvgeniyRuban <eugene_1997@inbox.ru>
2023-02-08 20:46:16 +0600

    Added unit tests project.

---
+++ NewHabr.UnitTests/ArticleTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NewHabr.Business.Services;
+using NewHabr.DAL.EF;
+using NewHabr.DAL.Repository;
+using NewHabr.Domain.Contracts;
+using NewHabr.Domain.Exceptions;
+using NewHabr.Domain.Models;
+
+namespace NewHabr.UnitTests;
+
+public class ArticleTests
+{
+    private IRepositoryManager _repositoryManager;
+    private IMapper _mapper;
+    private ApplicationContext _context;
+    
+    [Fact]
+    public async Task Test1()
+    {
+        _context = Factories.GetDataContext();
+        _repositoryManager = new RepositoryManager(_context);
+        var mapperConfigurations = new MapperConfiguration(config => config.AddMaps(typeof(Article).Assembly));
+        _mapper = mapperConfigurations.CreateMapper();
+
+        var articleService = new ArticleService(_repositoryManager, _mapper);
+
+        var guid = Guid.NewGuid();
+        _context.Articles.Add(new Article { Id = guid, Title = "" });
+        _context.SaveChanges();
+
+        await Assert.ThrowsAsync<ArticleNotFoundException>(async () => await articleService.GetByIdAsync(guid));
+    }
+}
---
+++ NewHabr.UnitTests/Factories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewHabr.DAL.EF;
+
+namespace NewHabr.UnitTests;
+public static class Factories
+{
+    public static ApplicationContext GetDataContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationContext>()
+                     .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                     .Options;
+        var db = new ApplicationContext(options);
+        return db;
+    }
+}
