0a49ec9dfda785cd90452474d27adc9b9e4517c5
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2023-02-15 20:41:50 +0500

    add mssql migrations

---
+++ 03. DAL/NewHabr.MSSQL/Migrations/20230215154118_ChangeLikeTables.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewHabr.MSSQL.Migrations
+{
+    public partial class ChangeLikeTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_AspNetUsers_UserId",
+                table: "Articles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserNotifications_AspNetUsers_UserId",
+                table: "UserNotifications");
+
+            migrationBuilder.DropTable(
+                name: "LikedArticles");
+
+            migrationBuilder.DropTable(
+                name: "LikedComments");
+
+            migrationBuilder.DropTable(
+                name: "LikedUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserNotifications_UserId",
+                table: "UserNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "UserNotifications");
+
+            migrationBuilder.CreateTable(
+                name: "NotificationUser",
+                columns: table => new
+                {
+                    NotificationsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationUser", x => new { x.NotificationsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_NotificationUser_AspNetUsers_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NotificationUser_UserNotifications_NotificationsId",
+                        column: x => x.NotificationsId,
+                        principalTable: "UserNotifications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLikesArticle",
+                columns: table => new
+                {
+                    LikedArticlesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LikesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLikesArticle", x => new { x.LikedArticlesId, x.LikesId });
+                    table.ForeignKey(
+                        name: "FK_UserLikesArticle_Articles_LikedArticlesId",
+                        column: x => x.LikedArticlesId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserLikesArticle_AspNetUsers_LikesId",
+                        column: x => x.LikesId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLikesAuthor",
+                columns: table => new
+                {
+                    LikedUsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReceivedLikesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLikesAuthor", x => new { x.LikedUsersId, x.ReceivedLikesId });
+                    table.ForeignKey(
+                        name: "FK_UserLikesAuthor_AspNetUsers_LikedUsersId",
+                        column: x => x.LikedUsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserLikesAuthor_AspNetUsers_ReceivedLikesId",
+                        column: x => x.ReceivedLikesId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLikesComment",
+                columns: table => new
+                {
+                    LikedCommentsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LikesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLikesComment", x => new { x.LikedCommentsId, x.LikesId });
+                    table.ForeignKey(
+                        name: "FK_UserLikesComment_AspNetUsers_LikesId",
+                        column: x => x.LikesId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserLikesComment_Comments_LikedCommentsId",
+                        column: x => x.LikedCommentsId,
+                        principalTable: "Comments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("00a98c8e-6a15-4447-9343-063f4f1efefc"),
+                column: "ConcurrencyStamp",
+                value: "0193a15a-83ff-4049-953a-7393fe5d7492");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("1bfc496b-ebd2-4c5a-b3e8-4b2c1e334391"),
+                column: "ConcurrencyStamp",
+                value: "212844f9-7f66-427f-a49c-013ce0ae86dd");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("aec1eede-5f3f-43ba-9ec3-454a3002c013"),
+                column: "ConcurrencyStamp",
+                value: "5de83041-ee5d-444d-8b5d-10e5073a4a30");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationUser_UsersId",
+                table: "NotificationUser",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLikesArticle_LikesId",
+                table: "UserLikesArticle",
+                column: "LikesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLikesAuthor_ReceivedLikesId",
+                table: "UserLikesAuthor",
+                column: "ReceivedLikesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLikesComment_LikesId",
+                table: "UserLikesComment",
+                column: "LikesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_AspNetUsers_UserId",
+                table: "Articles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_AspNetUsers_UserId",
+                table: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "NotificationUser");
+
+            migrationBuilder.DropTable(
+                name: "UserLikesArticle");
+
+            migrationBuilder.DropTable(
+                name: "UserLikesAuthor");
+
+            migrationBuilder.DropTable(
+                name: "UserLikesComment");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "UserNotifications",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "LikedArticles",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ArticleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LikedArticles", x => new { x.UserId, x.ArticleId });
+                    table.ForeignKey(
+                        name: "FK_LikedArticles_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LikedArticles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LikedComments",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CommentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LikedComments", x => new { x.UserId, x.CommentId });
+                    table.ForeignKey(
+                        name: "FK_LikedComments_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LikedComments_Comments_CommentId",
+                        column: x => x.CommentId,
+                        principalTable: "Comments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LikedUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LikedUsers", x => new { x.UserId, x.AuthorId });
+                    table.ForeignKey(
+                        name: "FK_LikedUsers_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LikedUsers_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("00a98c8e-6a15-4447-9343-063f4f1efefc"),
+                column: "ConcurrencyStamp",
+                value: "8d4d3605-9c0a-4e64-aa64-facb0e79174b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("1bfc496b-ebd2-4c5a-b3e8-4b2c1e334391"),
+                column: "ConcurrencyStamp",
+                value: "6986b2ea-7722-418a-9eee-a5df79fd5f2c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("aec1eede-5f3f-43ba-9ec3-454a3002c013"),
+                column: "ConcurrencyStamp",
+                value: "a12e5c20-6352-41f0-b17e-fd29d833b410");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserNotifications_UserId",
+                table: "UserNotifications",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LikedArticles_ArticleId",
+                table: "LikedArticles",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LikedComments_CommentId",
+                table: "LikedComments",
+                column: "CommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LikedUsers_AuthorId",
+                table: "LikedUsers",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_AspNetUsers_UserId",
+                table: "Articles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserNotifications_AspNetUsers_UserId",
+                table: "UserNotifications",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
