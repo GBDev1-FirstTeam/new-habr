a2c11db09bec4b7c3d63bad75bd8f4b671df6352
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2023-02-16 21:10:25 +0500

    fix: range dataannotation at userprofiledto

--- 02. Business/NewHabr.Domain/Dto/UserForManipulationDto.cs
+++ 02. Business/NewHabr.Domain/Dto/UserForManipulationDto.cs
@@ -15,6 +15,7 @@ public class UserForManipulationDto
     [MaxLength(30)]
     public string? Patronymic { get; set; }
 
+    [Range(-62135596800000, 253402300799999)]
     public long? BirthDay { get; set; }
 
     [MaxLength(200)]
