333ddc81d194e3d6714234af1378bfe0109a4165
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2023-02-18 22:54:00 +0500

    feat: add change username and/or password

--- 02. Business/NewHabr.Domain/Dto/RegistrationRequest.cs
+++ 02. Business/NewHabr.Domain/Dto/RegistrationRequest.cs
@@ -1,17 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿#nullable disable
+using System.ComponentModel.DataAnnotations;
 
 namespace NewHabr.Domain.Dto;
 
-#nullable disable
 public class RegistrationRequest
 {
-    [Required(ErrorMessage = "UserName is required")]
+    [Required(ErrorMessage = "UserName is required"), MinLength(3), MaxLength(20)]
+    [RegularExpression("[a-zA-Z0-9]+", ErrorMessage = "Allowed characters are: a-z and numbers")]
     public string UserName { get; set; }
 
     [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; }
 
     [Required(ErrorMessage = "Secure question is required")]
+    [Range(1, 1000)]
     public int SecurityQuestionId { get; set; }
 
     [MinLength(1)]
