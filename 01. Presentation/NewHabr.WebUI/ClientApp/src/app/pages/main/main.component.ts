cdc115f8f0b39e37195a381083bace8b8cf880cc
Danil Shabanov <shabanov1903@gmail.com>
2023-02-26 14:32:25 +0300

    Add info about banned

--- 01. Presentation/NewHabr.WebUI/ClientApp/src/app/pages/main/main.component.ts
+++ 01. Presentation/NewHabr.WebUI/ClientApp/src/app/pages/main/main.component.ts
@@ -3,12 +3,14 @@ import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Authorization } from 'src/app/core/models/Authorization';
 import { UserInfo } from 'src/app/core/models/User';
+import { ConvertDatePipe } from 'src/app/core/pipes/convert-date.pipe';
 import { AppStoreProvider } from 'src/app/core/store/store';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  providers: [ConvertDatePipe]
 })
 export class MainComponent {
 
@@ -19,6 +21,8 @@ export class MainComponent {
   isModerator: boolean;
   
   userInfo: UserInfo | null;
+
+  blockString: string;
   
   menu = [
     {
@@ -43,13 +47,23 @@ export class MainComponent {
     }
   ]
 
-  constructor(private store: AppStoreProvider, private router: Router) { }
+  constructor(private store: AppStoreProvider, private router: Router, private convertDate: ConvertDatePipe) { }
 
   ngOnInit(): void {
     const authSubscribtion = this.store.getAuth().subscribe(auth => this.auth = auth);
     const isAuthSubscribtion = this.store.getIsAuth().subscribe(isAuth => this.isAuth = isAuth);
     const isModeratorSubscribtion = this.store.getIsModerator().subscribe(isModerator => this.isModerator = isModerator);
-    const userInfoSubscribtion = this.store.getUserInfo().subscribe(userInfo => this.userInfo = userInfo);
+    const userInfoSubscribtion = this.store.getUserInfo().subscribe(userInfo => {
+      this.userInfo = userInfo;
+      
+      if (userInfo?.Banned) {
+        this.blockString = `Аккаунт заблокирован с ${this.convertDate.transform(userInfo?.BannedAt)} по ${this.convertDate.transform(userInfo?.BanExpiratonDate)}. Причина: ${userInfo?.BanReason}`;
+        setTimeout(() => {
+          const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]') as any;
+          const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+        }, 1000);
+      }
+    });
 
     this.subscribtions.push(authSubscribtion);
     this.subscribtions.push(isAuthSubscribtion);
