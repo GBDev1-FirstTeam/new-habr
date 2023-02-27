217f100e0e5c05aa7be3c00eeba69c25222e22e3
Ivan-Dell <vanbog@mail.ru>
2023-02-28 01:38:58 +0300

    added tests for comments controller

---
+++ CommentsTests/CommentsControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using NewHabr.WebApi.Controllers;
+
+namespace CommentsTests;
+
+public class CommentsControllerTests
+{
+    private CommentsController _commentsController;
+    public CommentsControllerTests()
+    {
+        _commentsController = new CommentsController();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
