82983b7427eedc1793eb9c11fbd782c19694479f
Simeon Iksanov <simeon.iksanov@gmail.com>
2023-03-19 13:46:31 +0500

    refactor article controller test

--- 04. Tests/UnitTests/Controllers/ArticlesControllerTests.cs
+++ 04. Tests/UnitTests/Controllers/ArticlesControllerTests.cs
@@ -1,75 +1,127 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using Microsoft.Extensions.Logging;
 using Moq;
 using NewHabr.Domain.Contracts;
 using NewHabr.Domain.Dto;
 using NewHabr.WebApi.Controllers;
 using Microsoft.AspNetCore.Http;
+using AutoFixture;
 using Microsoft.AspNetCore.Authorization;
-using NewHabr.Domain;
 
 namespace UnitTests.Controllers;
 
 public class ArticlesControllerTests
 {
-    private Mock<IArticleService> _articleServiceMock;
-    private readonly Mock<IAuthorizationService> _authorizationService;
-    private Mock<ILogger<ArticlesController>> _loggerMock;
-    private readonly ArticlesController _sut;
-
-
-    public ArticlesControllerTests()
+    public class CreateArticle : ControllerTestsBase
     {
-        _loggerMock = new();
-        _authorizationService = new Mock<IAuthorizationService>();
-        _articleServiceMock = new();
+        private readonly Mock<IArticleService> _articleServiceMock;
+        private readonly ArticlesController _sut;
 
-        _authorizationService
-            .Setup(s => s.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<Guid>(), It.IsAny<string>()))
-            .ReturnsAsync(() => AuthorizationResult.Success());
 
-        _sut = new ArticlesController(_articleServiceMock.Object, _authorizationService.Object, _loggerMock.Object);
+        public CreateArticle()
+        {
+            _articleServiceMock = new();
+
+            _sut = new ArticlesController(_articleServiceMock.Object, _authorizationService.Object, _loggerMock.Object);
+        }
+
+
+        [Fact]
+        public async Task SHOULD_return_created_article_and_StatusCode_201()
+        {
+            // Arrange
+            var request = _fixture.Create<ArticleCreateRequest>();
+
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Name, "unittestUser"),
+            };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+            _sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+            var articleDto = new ArticleDto();
+            _articleServiceMock
+                .Setup(s => s.CreateAsync(request, It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(articleDto);
+
+            // Act
+            var response = await _sut.Create(request, default);
+
+            // Assert
+            Assert.IsAssignableFrom<ActionResult<ArticleDto>>(response);
+
+            Assert.IsAssignableFrom<ObjectResult>(response.Result);
+            Assert.Equal(StatusCodes.Status201Created, ((ObjectResult)response.Result!).StatusCode);
+
+            _articleServiceMock.Verify(service => service.CreateAsync(It.IsAny<ArticleCreateRequest>(), It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
     }
 
-
-    [Fact]
-    public async Task CreateArticle_ModelWoCategoriesTags_ReturnsStatusCode201()
+    public class GetById : ControllerTestsBase
     {
-        // Arrange
-        var request = new ArticleCreateRequest
+        private Mock<IArticleService> _articleServiceMock;
+        private readonly ArticlesController _sut;
+
+
+        public GetById()
         {
-            Title = "unittest title",
-            Content = "unittest content",
-            ImgURL = "unittest imgurl"
-        };
+            _articleServiceMock = new();
+            _sut = new ArticlesController(_articleServiceMock.Object, _authorizationService.Object, _loggerMock.Object);
+        }
 
-        var claims = new Claim[]
+        [Fact]
+        public void SHOULD_call_service_WHEN_user_is_authorized()
         {
-            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
-            new Claim(ClaimTypes.Name, "unittestUser"),
-        };
-        var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
-        _sut.ControllerContext = new ControllerContext
+            // Arrange
+            var userId = _fixture.Create<Guid>();
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Name, "unittestUser"),
+            };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+            _sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+            var articleId = _fixture.Create<Guid>();
+
+            _articleServiceMock
+                .Setup(s => s.GetByIdAsync(articleId, userId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ArticleDto());
+
+            // Act
+            var articledto = _sut.GetById(articleId, default);
+
+            // Assert
+            _articleServiceMock
+                .Verify(s => s.GetByIdAsync(articleId, userId, It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public void SHOULD_call_service_WHEN_user_is_not_authorized()
         {
-            HttpContext = new DefaultHttpContext { User = user }
-        };
-        var articleDto = new ArticleDto();
-        _articleServiceMock
-            .Setup(s => s.CreateAsync(request, It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(articleDto);
+            // Arrange
+            _sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = new ClaimsPrincipal() }
+            };
+            var articleId = _fixture.Create<Guid>();
 
-        // Act
-        var response = await _sut.Create(request, default);
+            _articleServiceMock
+                .Setup(s => s.GetByIdAsync(articleId, Guid.Empty, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ArticleDto());
 
-        // Assert
-        Assert.IsAssignableFrom<ActionResult<ArticleDto>>(response);
+            // Act
+            var articledto = _sut.GetById(articleId, default);
 
-        Assert.IsAssignableFrom<ObjectResult>(response.Result);
-        Assert.Equal(StatusCodes.Status201Created, ((ObjectResult)response.Result!).StatusCode);
-
-        _articleServiceMock.Verify(service => service.CreateAsync(It.IsAny<ArticleCreateRequest>(), It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
+            // Assert
+            _articleServiceMock
+                .Verify(s => s.GetByIdAsync(articleId, Guid.Empty, It.IsAny<CancellationToken>()), Times.Once());
+        }
     }
 }
 
